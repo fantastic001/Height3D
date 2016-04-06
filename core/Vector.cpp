52e7e4973f2f06e4559fbc18cc345d37a4c5e08f
Stefan Nožinić <stefan@lugons.org>
2016-04-06 22:17:15 +0200

    Implement Vector

---
+++ core/Vector.cpp
@@ -0,0 +1,87 @@
+
+#include "Vector.hpp" 
+
+
+Vector::Vector(float x1, float y1, float z1, float w1) 
+{
+	column = true; 
+	x = x1; 
+	y = y1; 
+	z = z1; 
+	w = w1;
+}
+
+Vector::Vector(float x1, float y1, float z1, float w1, bool column1) 
+{
+	column = column1;
+	x = x1;
+	y = y1; 
+	z = z1; 
+	w = w1; 
+}
+
+Vector::Vector(float a) 
+{
+	column = true; 
+	x = a; 
+	y = a; 
+	z = a; 
+	w = a; 
+}
+
+Vector& Vector::operator= (Vector v) 
+{
+	x = v.getX();
+	y = v.getY(); 
+	z = v.getZ();
+	w = v.getW();
+	column = v.isColumn();
+}
+
+Vector& Vector::operator+ (Vector v) 
+{
+	return *(new Vector(x + v.getX(), y + v.getY(), z + v.getZ(), w + v.getW(), column));
+}
+Vector& Vector::operator- (Vector v) 
+{
+	return *(new Vector(x - v.getX(), y - v.getY(), z - v.getZ(), w - v.getW(), column));
+}
+	
+float Vector::dot(Vector v) 
+{
+	return x*v.getX() + y*v.getY() + z*v.getZ() + w*v.getW();
+}
+Vector Vector::transpose() 
+{
+	return Vector(x,y,z,w,!column);
+}
+
+float Vector::getX() 
+{
+	return x;
+}
+
+float Vector::getY() 
+{
+	return y; 
+}
+
+float Vector::getZ() 
+{
+	return z;
+}
+
+float Vector::getW() 
+{
+	return w; 
+}
+
+bool Vector::isColumn() 
+{
+	return column;
+}
+
+Vector Vector::zero() 
+{
+	return Vector(0.0);
+}
---
+++ core/Vector.hpp
@@ -0,0 +1,31 @@
+
+#ifndef HEIGHT3D_VECTOR_HPP
+#define HEIGHT3D_VECTOR_HPP
+
+class Vector 
+{
+	float x,y,z,w; 
+	bool column; 
+
+public: 
+	Vector(float x1, float y1, float z1, float w1); 
+	Vector(float x1, float y1, float z1, float w1, bool column1);
+	Vector(float a); 
+
+	Vector& operator= (Vector v); 
+	Vector& operator+ (Vector v);
+	Vector& operator- (Vector v); 
+	
+	float dot(Vector v); 
+	Vector transpose();
+
+	float getX();
+	float getY();
+	float getZ();
+	float getW();
+	bool isColumn();
+
+	static Vector zero(); 
+};
+
+#endif // HEIGHT3D_VECTOR_HPP
