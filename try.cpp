b4cd3a1357b1a2e33b6b2f9aad30e76cfb98a236
Stefan Nožinić <stefan@lugons.org>
2016-04-06 22:20:06 +0200

    Use new stuff

--- try.cpp
+++ try.cpp
@@ -11,112 +11,68 @@
 #include "shaders.hpp"
 #include "sdl_init.hpp"
 
+#include "core/Buffer.hpp"
+#include "core/Vector.hpp"
+
+#include "gui/Window.hpp"
+
 using namespace std; 
 
 int main(int argc, char *argv[])
 {
-    SDL_Window *mainwindow = sdl_init("tutorial");
-    
-    /* Clear our buffer with a red background */
-    glClearColor ( 1.0, 0.0, 0.0, 1.0 );
-    glClear ( GL_COLOR_BUFFER_BIT );
-    /* Swap our back buffer to the front */
-    SDL_GL_SwapWindow(mainwindow);
-    /* Wait 2 seconds */
-    SDL_Delay(2000);
+	Window win(512, 512, "tutorial");
+	
+	glClearColor ( 1.0, 0.0, 0.0, 1.0 );
+	glClear ( GL_COLOR_BUFFER_BIT );
+	win.redraw();
+	win.delay(2000);
+	
+	glClearColor ( 0.0, 1.0, 0.0, 1.0 );
+	glClear ( GL_COLOR_BUFFER_BIT );
+	win.redraw();
+	win.delay(2000);
+	
+	glClearColor ( 0.0, 0.0, 1.0, 1.0 );
+	glClear ( GL_COLOR_BUFFER_BIT );
+	win.redraw();
+	win.delay(2000);
+	
+	glewInit();
+	Program program;
+	program.addShader(Shader(GL_VERTEX_SHADER, "shader.vp"));
+	program.addShader(Shader(GL_FRAGMENT_SHADER, "shader.fp"));
+	program.link();
+	program.bind();
 
-    /* Same as above, but green */
-    glClearColor ( 0.0, 1.0, 0.0, 1.0 );
-    glClear ( GL_COLOR_BUFFER_BIT );
-    SDL_GL_SwapWindow(mainwindow);
-    SDL_Delay(2000);
+	int loc = program.getAttributeLocation("vPosition");
+	program.enableAttributeArray(loc);
+	Vector vertices[3] = {
+		Vector(-1.0, -1.0, 0.0, 0.0),
+		Vector(1.0, -1.0, 0.0, 0.0),
+		Vector(0.0, 1.0, 0.0, 0.0)
+	};
+	GLfloat g_vertex_buffer_data[] = {
 
-    /* Same as above, but blue */
-    glClearColor ( 0.0, 0.0, 1.0, 1.0 );
-    glClear ( GL_COLOR_BUFFER_BIT );
-    SDL_GL_SwapWindow(mainwindow);
-    SDL_Delay(2000);
-    cout << "Compiling...\n";
+		-1.0f, -1.0f, 0.0f,
 
-    // draw red triangle 
-    Program program = initProgram();
+		1.0f, -1.0f, 0.0f,
 
-    cout << "Drawing triangle\n";
+		0.0f,  1.0f, 0.0f,
+	
+	}; 
 
-    GLuint VertexArrayID;
-
-    glGenVertexArrays(1, &VertexArrayID);
-
-    glBindVertexArray(VertexArrayID);
-    
- static const GLfloat g_vertex_buffer_data[] = {
-
-    -1.0f, -1.0f, 0.0f,
-
-    1.0f, -1.0f, 0.0f,
-
-    0.0f,  1.0f, 0.0f,
-
- }; 
-
-
-
- // This will identify our vertex buffer
-
- GLuint vertexbuffer;
-
- // Generate 1 buffer, put the resulting identifier in vertexbuffer
-
- glGenBuffers(1, &vertexbuffer);
-
- // The following commands will talk about our 'vertexbuffer' buffer
-
- glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-
- // Give our vertices to OpenGL.
-
- glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-   
-
- // 1rst attribute buffer : vertices
-
- glEnableVertexAttribArray(0);
-
- glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-
- glVertexAttribPointer(
-
-    0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-
-    3,                  // size
-
-    GL_FLOAT,           // type
-
-    GL_FALSE,           // normalized?
-
-    0,                  // stride
-
-    (void*)0            // array buffer offset
-
- );
-
- // Draw the triangle !
- cout << "Drawing primitives\n";
-
- glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
-
-
-    // --------------------------------------------------------------------------------------
-    
-    
-    SDL_GL_SwapWindow(mainwindow);
-    SDL_Delay(2000);
-    
-    cout << "Exiting...\n";
-    /* Delete our opengl context, destroy our window, and shutdown SDL */
-    //SDL_GL_DeleteContext(maincontext);
-    SDL_DestroyWindow(mainwindow);
-    SDL_Quit();
-
-    return 0;
+	Buffer buf(GL_ARRAY_BUFFER);
+	buf.sendData(vertices, 3, false);
+	buf.bind();
+	
+	program.setAttributeArray(loc, 3);
+	
+	glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+	
+	win.redraw();
+	win.delay(2000);
+	
+	SDL_Quit();
+	
+	return 0;
 }
