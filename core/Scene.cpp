894a60e12842f815fe2b8f8dc4ffba67e3bb3503
Stefan Nožinić <stefan@lugons.org>
2016-04-15 01:51:24 +0200

    Implement Scene

---
+++ core/Scene.cpp
@@ -0,0 +1,83 @@
+
+#include "common.hpp"
+#include "Scene.hpp"
+
+using namespace std;
+
+Scene::Scene(float a, float b, float c) 
+{
+	m_width = a; 
+	m_height = b; 
+	m_depth = c;
+}
+
+Scene::~Scene() 
+{
+	for (int i = 0; i<objects.size(); i++) 
+	{
+		delete m_objects.at(i);
+	}
+}
+
+SceneObject* Scene::addObject(SceneObject* object) 
+{
+	m_objects.push_back(object);
+	return object;
+}
+
+void Scene::setCamera(float x, float y, float z, Vector direction, Vector up) 
+{
+	m_cx = x; 
+	m_cy = y; 
+	m_cz = z;
+	m_direction = direction;
+	m_up = up;
+}
+
+void Scene::setPerspective(float fovy, float aspect, float near, float far) 
+{
+	m_fovy = fovy;
+	m_aspect = aspect; 
+	m_near = near; 
+	m_far = far;
+}
+
+Matrix Scene::getPerspectiveMatrix(float x, float y, float z, Vector direction, Vector up, float fovy, float aspect, float near, float far) 
+{
+	// first we set camera params
+	direction = direction.normalized();
+	Vector normal = direction.cross(up).normalized();
+	Vector up = normal.cross(direction).normalized();
+	direction = Vector(-direction.getX(), -direction.getY(), -direction.getZ());
+	Vector eye(x,y,z);
+	Matrix camera(
+		normal.getX(), normal.getY(), normal.getZ(), -normal.dot(eye),
+		up.getX(), up.getY(), up.getZ(), -up.dot(eye), 
+		direction.getX(), direction.getY(), direction.getZ(), -direction.dot(eye),
+		0, 0, 0, 1
+	);
+	
+	float f = 1.0 / tan(fovy / 2);
+	float d = far - near;
+	Matrix perspective(
+		f/aspect, 0, 0, 0, 
+		0, f, 0, 0,
+		0 0, -(near + far)/d, -2*near*far/d, 
+		0, 0, -1, 0.0
+	);
+	Matrix result = perspective.mult(camera);
+	return result;
+}
+	
+void Scene::drawObjects(Program *prog, int modelUniformMatrixLocation, int perspectiveMatrixLocation, int vLocation, int texCoordLocation) 
+{
+	prog->setUniformValue(PerspectiveMatrixLocation, getPerspectiveMatrix(m_cx, m_cy, m_cz, m_direction, m_up, m_fovy, m_aspect, m_near, m_far));
+	for (int i = 0; i<m_objects.size(); i++) 
+	{
+		SceneObject *obj = m_objects.at(i);
+		obj->setAttributes(prog, vLocation, texCoordLocation, modelUniformMatrixLocation);
+		obj->bindTexture();
+		obj->bindIndexBuffer();
+		obj->draw();
+	}
+}
--- core/Scene.hpp
+++ core/Scene.hpp
@@ -2,24 +2,54 @@
 #ifndef HEIGHT3D_SCENE_HPP
 #define HEIGHT3D_SCENE_HPP
 
+#include <vector>
+
+#include "SceneObject.hpp"
+#include "AbstractModel.hpp"
+#include "Matrix.hpp"
+#include "Vector.hpp"
+#include "Program.hpp"
+
 class Scene 
 {
+	// scene parameters 
+	std::vector<SceneObject*> m_objects; 
+	float m_fovy, m_aspect, m_near, m_far;
+	
+	// camera parameters
+	float m_cx, float m_cy, float m_cz;
+	Vector m_direction;
+	Vector m_up;
+
+	// perspective parameters 
+	float m_fovy;
 
 public:
 	Scene(float a, float b, float c);
+	~Scene();
 
-	// addObject(AbstractModel model, float x, float y, float z, float alpha, float phi, float a, float b, float c);
+	SceneObject* addObject(SceneObject* object);
 	// addLight(AbstractLight, float x, float y, float z);
-	// setCamera(float x, float y, float z, float lx, float ly, float lz);
-	// getModelMatrix(float x, float y, float z, float alpha, float phi, float a, float b, float c);
-	// getPerspective(float x, float y, float z, float lx, float ly, float lz, float fovy);
+	void setCamera(float x, float y, float z, Vector direction, Vector up);
 	
-	// getMatrix(
-	//		float x, float y, float z, float alpha, float phi, float a, float b, float c,
-	//		float cx, float cy, float cz, float lx, float ly, float lz, float fovy
-	//		);
+	/*
+	Sets perspective parameters
 
-	// void drawObjects(int modelUniformMatrixLocation, int PerspectiveMatrixLocation);
+	NOTE: fovy is given in radians
+	*/
+	void setPerspective(float fovy, float aspect, float near, float far);
+	
+
+	/*
+	Return matrix to project scene object into camera space with perspective projection
+
+	NOTE: fovy is given in radians 
+	NoTE 2: aspect is your aspect ratio
+	*/
+	static Matrix getPerspectiveMatrix(float x, float y, float z, Vector direction, Vector up, float fovy, float aspect, float near, float far);
+	
+
+	void drawObjects(Program* prog, int modelUniformMatrixLocation, int PerspectiveMatrixLocation);
 };
 
 #endif // HEIGHT3D_SCENE_HPP
