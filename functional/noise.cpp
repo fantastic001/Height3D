64298deb4fc211266d77dbed4ca9eba03c48b0ba
Stefan Nožinić <stefan@lugons.org>
2016-05-27 19:17:09 +0200

    Functional test for Perlin noise

---
+++ functional/noise.cpp
@@ -0,0 +1,22 @@
+#include <boost/test/unit_test.hpp>
+
+#include <iostream>
+
+#define GL3_PROTOTYPES 1
+#include <GL/glew.h>
+
+#include <core/Vector.hpp>
+
+#include <SDL2/SDL.h>
+
+#include <image/PerlinImageAdapter.hpp>
+
+using namespace std;
+
+BOOST_AUTO_TEST_CASE( noise )
+{
+	PerlinImageAdapter adapter(new Perlin(16));
+	adapter.saveToFile("img.png", 32);
+}
+
+
