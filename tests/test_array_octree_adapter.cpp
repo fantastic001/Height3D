1e878bfbfcc1b1fc807dac0b150a71d322bcd025
Stefan Nožinić <stefan@lugons.org>
2016-06-23 01:07:28 +0200

    Initialize array in test case

--- tests/test_array_octree_adapter.cpp
+++ tests/test_array_octree_adapter.cpp
@@ -9,6 +9,7 @@
 BOOST_AUTO_TEST_CASE( array_octree_test )
 {
 	Array3D<bool> a(3,3,3);
+	for (int i = 0; i<3; i++) for (int j = 0; j<3; j++) for (int k = 0; k<3; k++) a(i,j,k) = false; 
 	a(1,1,1) = true;
 	Array3DOctreeAdapter adapter(a);
 	OctreeNode* root = adapter.getRoot();
