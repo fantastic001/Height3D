998ab6cc3f088737350646621fbb99d7d5a794d7
Stefan Nožinić <stefan@lugons.org>
2016-07-06 21:43:34 +0200

    Fix bugs in octree generation

--- src/structures/Array3DOctreeAdapter.cpp
+++ src/structures/Array3DOctreeAdapter.cpp
@@ -13,15 +13,31 @@ Octree* Array3DOctreeAdapter::addChilds(Array3D<bool> &arr, int startx, int endx
 	bool active = arr(x,y,z);
 	float value = active ? 1.0 : 0.0;
 	Octree *node = new Octree(position,value, active);
-	node->setChild(false, false, false, addChilds(arr, startx, x, starty, y, startz, z));
-	node->setChild(false, false, true, addChilds(arr, startx, x, starty, y, z+1,  endz));
-	node->setChild(false, true, false, addChilds(arr, startx, x, y+1, endy, startz, z));
-	node->setChild(false, true, true, addChilds(arr, startx, x, y+1, endy, z+1, endz));
-	
-	node->setChild(true, false, false, addChilds(arr, x+1, endx, starty, y, startz, z));
-	node->setChild(true, false, true, addChilds(arr, x+1, endx, starty, y, z+1,  endz));
-	node->setChild(true, true, false, addChilds(arr, x+1, endx, y+1, endy, startz, z));
-	node->setChild(true, true, true, addChilds(arr, x+1, endx, y+1, endy, z+1, endz));
+	if (endx - startx == 1 || endy - starty == 1 || endz - startz == 1) 
+	{
+		
+		node->setChild(false, true, false, NULL);
+		node->setChild(false, true, true, NULL);
+		node->setChild(false, false, false, NULL);
+		node->setChild(false, false, true, NULL);
+
+		node->setChild(true, true, false, NULL);
+		node->setChild(true, true, true, NULL);
+		node->setChild(true, false, false, NULL);
+		node->setChild(true, false, true, NULL);
+
+	}
+	else {
+		node->setChild(false, false, false, addChilds(arr, startx, x, starty, y, startz, z));
+		node->setChild(false, false, true, addChilds(arr, startx, x, starty, y, z,  endz));
+		node->setChild(false, true, false, addChilds(arr, startx, x, y, endy, startz, z));
+		node->setChild(false, true, true, addChilds(arr, startx, x, y, endy, z, endz));
+		
+		node->setChild(true, false, false, addChilds(arr, x, endx, starty, y, startz, z));
+		node->setChild(true, false, true, addChilds(arr, x, endx, starty, y, z,  endz));
+		node->setChild(true, true, false, addChilds(arr, x, endx, y, endy, startz, z));
+		node->setChild(true, true, true, addChilds(arr, x, endx, y, endy, z, endz));
+	}
 	return node; 
 }
 
