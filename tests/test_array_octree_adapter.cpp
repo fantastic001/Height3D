b9b9873dfcd99e033603cb0b5d12510571f209d8
Stefan Nožinić <stefan@lugons.org>
2016-07-07 00:30:00 +0200

    Add additional unit test

--- tests/test_array_octree_adapter.cpp
+++ tests/test_array_octree_adapter.cpp
@@ -17,3 +17,17 @@ BOOST_AUTO_TEST_CASE( array_octree_test )
 	BOOST_CHECK( !root->getChild(true, true, true)->active());
 	BOOST_CHECK( root->getChild(true, true, true)->getChild(true, true, true) == NULL);
 }
+
+BOOST_AUTO_TEST_CASE( array_octree_large_test )
+{
+	int i,j,k;
+
+	Array3D<bool> a(5,5,5);
+	for (i = 0; i<3; i++) for (j = 0; j<3; j++) for (k = 0; k<3; k++) a(i,j,k) = false; 
+	a(2,2,2) = true;
+	Array3DOctreeAdapter adapter(a);
+	OctreeNode* root = adapter.getRoot();
+	BOOST_CHECK(root->active());
+	BOOST_CHECK(! root->getChild(true, true, true)->active());
+	BOOST_CHECK( root->getChild(true, true, true)->getChild(true, true, true) == NULL);
+}
