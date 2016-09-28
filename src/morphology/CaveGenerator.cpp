642d36c21936a20c8b8a07d6e162338733d2f122
Stefan Nožinić <stefan@lugons.org>
2016-09-28 23:26:07 +0200

    Fix layers after adding a new layer in cave generator

--- src/morphology/CaveGenerator.cpp
+++ src/morphology/CaveGenerator.cpp
@@ -64,4 +64,57 @@ void CaveGenerator::generate(Kernel *k, LayeredHeightfield *h)
 			}
 		}
 	}
+	// we now have to fix h(l,i,j) if it is on the edge to ommit holes in primary level 
+	bool*** change = new bool** [levels];
+	for (int l = 0; l<levels; l++) {
+		change[l] = new bool* [size+1];
+		for (i=0; i<=size; i++) 
+		{
+			change[l][i] = new bool [size+1];
+			for (j=0; j<=size; j++) change[l][i][j] = false; 
+		}
+	}
+	for (i=0; i<=size; i++) 
+	{
+		for (j=0; j<=size; j++) 
+		{
+			float x = h->toContinious(i);
+			float z = h->toContinious(j);
+			if (k->defined(x,z)) 
+			{
+				for (int l = 0; l<levels; l++) 
+				{
+					if (added && 
+						h->getBottom(levels+1,i,j) <= k->bottom(x,z) 
+						&& k->bottom(x,z) <= k->top(x,z) 
+						&& k->top(x,z) <= h->getTop(levels,i,j)
+					)
+					{
+						if (
+							(i<size && h->getTop(l,i+1,j) != -1) || 
+							(j<size && h->getTop(l,i,j+1) != -1) || 
+							(i>0 && h->getTop(l,i-1,j) != -1) || 
+							(j>0 && h->getTop(l,i,j-1) != -1) ||
+							
+							(i<size-1 && h->getTop(l,i+2,j) != -1) || 
+							(j<size-1 && h->getTop(l,i,j+2) != -1) || 
+							(i>1 && h->getTop(l,i-2,j) != -1) || 
+							(j>1 && h->getTop(l,i,j-2) != -1) 
+						
+						)
+						{
+							change[l][i][j] = true;
+						}
+					}
+				}
+			}
+		}
+	}
+	for (i=0; i<=size; i++)  for (j=0; j<=size; j++) for (int l = 0; l<levels; l++) if (change[l][i][j]) 
+	{
+		
+		h->setTop(l,i,j,h->getTop(levels,i,j));
+		h->setBottom(l,i,j,h->getBottom(levels+1, i,j));
+	}
+
 }
